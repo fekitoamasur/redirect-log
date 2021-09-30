47c959e8e13fe90c16831b3bc077be67e2945733
Wolf Arthur <wolf.arthur@gmail.com>
2021-09-30 19:56:22 +0200

    auto-commit of log.txt

--- redirect.js
+++ redirect.js
@@ -10,7 +10,7 @@ app.get("/proton-mail-secure-redirect", (req, res) => {
   const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
 
   // Log entry 
-  const entry = `At time ${Date.now()} [${new Date().toString()}] got request with ID «${req.query.id}» from IP:«${ip}»\n`;
+  const entry = `# At time ${Date.now()} [${new Date().toString()}] got request with ID «${req.query.id}» from IP:«${ip}»\n`;
   
   // Console log
   console.log(entry);
@@ -23,15 +23,16 @@ app.get("/proton-mail-secure-redirect", (req, res) => {
 
     // Match check 
   if (req.query.id == "EefangieGodosiegaenah5eunah4chi6") {
-    log("This is a match with the watche-for ID !!! The fish bit the hook, amazing...\n");
+    log("    *** This is a match with the watched-for ID !!! The fish bit the hook, amazing... ***\n");
     log_fucker();
   }
 
   // Log the full request 
-  log(JSON.stringify(req.headers) + "\n");
-  log(JSON.stringify(req.query) + "\n");
-  log(JSON.stringify([req.ip, req.ips]) + "\n");
-  log(JSON.stringify(geo) + "\n\n");
+  log(`    headers: ${JSON.stringify(req.headers)}\n`);
+  log(`    query: ${JSON.stringify(req.query)}\n`);
+  log(`    ips: ${JSON.stringify([req.ip, req.ips])}\n`);
+  log(`    geoloc: ${JSON.stringify(geo)}\n\n`);
+
 
   // Auto-commit 
   sh.exec(`git commit -a -m 'auto-commit of log.txt'`);
@@ -46,16 +47,16 @@ app.listen(port, () => {
   console.log(`Express app listening at http://localhost:${port}`);
 });
 
-// localhost:3128/proton-mail-secure-redirect?id=test
-
 function log(text) {
     fs.writeFileSync('log.txt', text, { flag: 'a+' });
 }
 
 function log_fucker() {
+    log("                                                                  \n");
     log("   ____       _     _   _             __            _             \n");
     log("  / ___| ___ | |_  | |_| |__   ___   / _|_   _  ___| | _____ _ __ \n");
     log(" | |  _ / _ \| __| | __| '_ \ / _ \ | |_| | | |/ __| |/ / _ \ '__|\n");
     log(" | |_| | (_) | |_  | |_| | | |  __/ |  _| |_| | (__|   <  __/ |   \n");
     log("  \____|\___/ \__|  \__|_| |_|\___| |_|  \__,_|\___|_|\_\___|_|   \n");
+    log("                                                                  \n");
 }
